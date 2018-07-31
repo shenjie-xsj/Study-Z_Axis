c0b9a3d620563dbb54bad3443fdbdef28edeeab7
EU_ShenJie <shenjie_xsj@yeah.net>
2018-07-31 10:09:18 +0800

    完成z轴调整部分的学习

--- Z_Axis/Z_Axis/ViewController.swift
+++ Z_Axis/Z_Axis/ViewController.swift
@@ -9,15 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var blueView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //通过更改CALayer的zPosition值来改变图层的覆盖关系(谁的zPosition越大，z轴就越高，显示的内容就越多（因为不会被覆盖）)
+        redView.layer.zPosition = 3.0
+        blueView.layer.zPosition = 2.0
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
